97e08f5bb67013781ed2d4b6024feb25a347a9ff
heifade <heifade@126.com>
2018-08-29 14:15:52 +0800

    no message

--- test/index.js
+++ test/index.js
@@ -1,8 +1,15 @@
 let { zipDir, unzipDir } = require("../dist");
 
+let fs = require("fs");
+
 async function f() {
-  await zipDir("../archive-dir", "../../zip.zip");
-  await unzipDir("../../zip.zip", "../../pp");
+  let zipText = "./zipTest";
+  if (!fs.existsSync(zipText)) {
+    fs.mkdirSync(zipText);
+  }
+
+  await zipDir("./src", `${zipText}/file.zip`);
+  await unzipDir(`${zipText}/file.zip`, `${zipText}`);
 }
 
 f()
